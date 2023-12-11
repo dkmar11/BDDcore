ffeed217f6e95d20a4a7cd1453ab92d5fe289c21
dkmar11 <israelzambrana@gmail.com>
2023-12-11 16:23:35 -0400

    test case @02 created

--- main/clickup/ui/task/create_task_page.js
+++ main/clickup/ui/task/create_task_page.js
@@ -2,31 +2,38 @@ const { By } = require("selenium-webdriver");
 const WebDriverActions = require("../../../core/utils/ui/web_driver_actions");
 const WebDriverWaitings = require("../../../core/utils/ui/web_driver_waitings");
 const logger = require("../../../core/utils/logger");
+const WebDriverConditions = require("../../../core/utils/ui/web_driver_conditions");
 /**
  * class to work with task page
  */
-class CreateTaskPage{
+class CreateTaskPage {
   /**
    * Constructor of the class where the selector are declared
    */
-  constructor(){
+  constructor() {
+    this.taskCreationWindow = By.css('cu-create-task[class="ng-star-inserted"]')
     this.newTaskButton = By.css('button[data-test="create-task-menu__new-task-button"]');
     this.titleInput = By.css('textarea[data-test="draft-view__title-task"]');
     this.descriptionInput = By.css('.ql-block[data-block-id*="block"]');
     this.summitButton = By.css('button[data-test="draft-view__quick-create-create"]');
-    this.addSubTaskButton = By.css('div[class*="add-item-subtask"], a[class*="subtasks-add"]')
-    this.subTaskControlButton = By.css('div[class*="subtasks-controls"]')
+    this.addSubTaskButton = By.css('div[class*="add-item-subtask"], a[class*="subtasks-add"]');
+    this.subTaskControlButton = By.css('div[class*="subtasks-controls"]');
+    this.taskNameError = By.css('div[cupendoid="quick-create-task-enter-task-name-error"]');
   }
   /**
-   * @param {Object} task - object that must contains the following task information:
-   * task.name - name of the task
-   * task.description - description of the task, default it is an empty string
+   * @param {Object} task - object that it will be filled into 
+   * - the task name input field (task.name)
+   * - the description input field (task.description)
+   * note: the new task button is clicked if it is not open the task creation window
+   * @returns {Boolean}
    */
-  async createTask(task){
+  async createTask(task) {
     logger.debug("Creating task");
-    await WebDriverActions.clickOnElement(this.newTaskButton);
+    if(!await WebDriverConditions.elementIsVisible(this.taskCreationWindow)){
+      await WebDriverActions.clickOnElement(this.newTaskButton);
+    }
     await WebDriverActions.sendKeys(this.titleInput, task.name);
-    await WebDriverActions.sendKeys(this.descriptionInput, task.description||"");
+    await WebDriverActions.sendKeys(this.descriptionInput, task.description);
   }
   /**
    * Method to add subtasks to a task
--- tests/support/ui_step_definitions/task/ui_task_assertion_steps.js
+++ tests/support/ui_step_definitions/task/ui_task_assertion_steps.js
@@ -1,6 +1,9 @@
 const { Then } = require("@cucumber/cucumber");
 const { expect } = require("expect")
 const taskPage = require("../../../../main/clickup/ui/task/task_page");
+const WebDriverWaitings = require("../../../../main/core/utils/ui/web_driver_waitings");
+const create_task_page = require("../../../../main/clickup/ui/task/create_task_page");
+const WebDriverGettings = require("../../../../main/core/utils/ui/web_driver_gettings");
 
 Then("the user should see on task page the following information:", async function(table) {
   let expectedData = {};
@@ -14,4 +17,9 @@ Then("the user should see on task page the following information:", async functi
   });
   const actualInformation = await taskPage.getTask();
   expect(actualInformation).toMatchObject(expectedData);
+});
+
+Then("{string} message should apper on the task creation window", async function(textExpected) {
+  await WebDriverWaitings.elementIsLocated(create_task_page.taskCreationWindow);
+  expect(await WebDriverGettings.getText(create_task_page.taskNameError)).toBe(textExpected);
 });
